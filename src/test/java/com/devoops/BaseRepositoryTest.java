06fc7888e8cc359e9f8c3d42469f9fd1fad85950
coli <kkwoo001021@naver.com>
2025-05-13 06:03:29 +0900

    style: 접근제어지시자 수정

--- src/test/java/com/devoops/BaseRepositoryTest.java
+++ src/test/java/com/devoops/BaseRepositoryTest.java
@@ -3,5 +3,6 @@ package com.devoops;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-abstract class BaseRepositoryTest {
+public abstract class BaseRepositoryTest {
+
 }
