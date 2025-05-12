82b9cd62a05ebe6d5291c98e0fc9fc82a24c0109
coli <kkwoo001021@naver.com>
2025-05-13 05:10:51 +0900

    chore: swagger 기초 세팅

---
+++ src/main/java/com/devoops/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.devoops.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(server())
+                .info(getInfo());
+    }
+
+    private Server server() {
+        return new Server().url("/");
+    }
+
+    private Info getInfo() {
+        return new Info().title("DevOops API 문서")
+                .version("1.0.0");
+    }
+}
+
