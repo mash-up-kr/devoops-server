5d483319a346d1366851f70494a72842ab7bae88
bbeomgeun <miki30808@gmail.com>
2025-05-23 05:50:14 +0900

    chore: 멀티 모듈 세팅 1차

---
+++ gss-mcp-app/src/main/java/com/devoops/GssMcpApplication.java
@@ -0,0 +1,13 @@
+package com.devoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GssMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GssMcpApplication.class, args);
+	}
+
+}
