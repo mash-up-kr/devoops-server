1a41a7402d830434070aadb225284993c1f7316c
coli <kkwoo001021@naver.com>
2025-06-24 03:18:43 +0900

    chore: 생략한 레포지토릭 구현 선언 추가

--- gss-domain/src/main/java/com/devoops/jpa/repository/github/QuestionDomainRepositoryImpl.java
+++ gss-domain/src/main/java/com/devoops/jpa/repository/github/QuestionDomainRepositoryImpl.java
@@ -1,11 +1,12 @@
 package com.devoops.jpa.repository.github;
 
+import com.devoops.domain.repository.github.QuestionDomainRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class QuestionDomainRepositoryImpl {
+public class QuestionDomainRepositoryImpl implements QuestionDomainRepository {
 
     private final QuestionJpaRepository questionRepository;
 }
