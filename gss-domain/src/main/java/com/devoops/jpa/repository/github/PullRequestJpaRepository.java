21a14f1b5d82f2819fdbc55914cf0518b8a80261
coli <kkwoo001021@naver.com>
2025-06-24 03:19:53 +0900

    fix: 잘못된 키 자료형 수정

--- gss-domain/src/main/java/com/devoops/jpa/repository/github/PullRequestJpaRepository.java
+++ gss-domain/src/main/java/com/devoops/jpa/repository/github/PullRequestJpaRepository.java
@@ -3,6 +3,6 @@ package com.devoops.jpa.repository.github;
 import com.devoops.jpa.entity.github.PullRequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PullRequestJpaRepository extends JpaRepository<PullRequestEntity, String> {
+public interface PullRequestJpaRepository extends JpaRepository<PullRequestEntity, Long> {
 
 }
