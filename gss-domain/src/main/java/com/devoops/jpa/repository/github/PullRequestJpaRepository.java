3ad0e42b32667f35ebc817ad003f3c884abe03e5
coli <kkwoo001021@naver.com>
2025-06-26 22:45:41 +0900

    style: 불필요한 공백 제거

--- gss-domain/src/main/java/com/devoops/jpa/repository/github/PullRequestJpaRepository.java
+++ gss-domain/src/main/java/com/devoops/jpa/repository/github/PullRequestJpaRepository.java
@@ -3,7 +3,6 @@ package com.devoops.jpa.repository.github;
 import com.devoops.jpa.entity.github.PullRequestEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PullRequestJpaRepository extends JpaRepository<PullRequestEntity, Long> {
