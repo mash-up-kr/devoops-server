447a92c58741344b5e3300c7155b606f40b6292e
coli <kkwoo001021@naver.com>
2025-06-27 03:38:01 +0900

    test: 페이지네이션 테스트 추가

--- gss-api-app/src/test/java/com/devoops/service/repository/RepositoryServiceTest.java
+++ gss-api-app/src/test/java/com/devoops/service/repository/RepositoryServiceTest.java
@@ -2,6 +2,7 @@ package com.devoops.service.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.devoops.BaseServiceTest;
 import com.devoops.domain.entity.github.GithubRepository;
@@ -47,6 +48,36 @@ class RepositoryServiceTest extends BaseServiceTest {
         }
 
         @Test
+        void 레포지토리_풀_리퀘스트_목록에_페이지네이션을_적용한다() {
+            LocalDateTime now = LocalDateTime.now();
+            User user = userGenerator.generate("김건우");
+            GithubRepository repo = repoGenerator.generate(user, "김건우의 레포지토리");
+            PullRequest fiveMinutesAgoPR = pullRequestGenerator.generate("5분전 PR", RecordStatus.PENDING, repo,
+                    now.minusMinutes(5L));
+            PullRequest threeMinutesAgoPR = pullRequestGenerator.generate("3분전 PR", RecordStatus.PENDING, repo,
+                    now.minusMinutes(3L));
+            PullRequest oneMinutesAgoPR = pullRequestGenerator.generate("1분전 PR", RecordStatus.PENDING, repo,
+                    now.minusMinutes(1L));
+
+            List<Long> pageOneIds = repositoryService.getPullRequestsByRepository(user, repo.getId(), 2, 0)
+                    .getValues()
+                    .stream()
+                    .map(PullRequest::getId)
+                    .toList();
+
+            List<Long> pageTwoIds = repositoryService.getPullRequestsByRepository(user, repo.getId(), 2, 1)
+                    .getValues()
+                    .stream()
+                    .map(PullRequest::getId)
+                    .toList();
+
+            assertAll(
+                    () -> assertThat(pageOneIds).containsOnly(oneMinutesAgoPR.getId(), threeMinutesAgoPR.getId()),
+                    () -> assertThat(pageTwoIds).containsOnly(fiveMinutesAgoPR.getId())
+            );
+        }
+
+        @Test
         void 주인이_아니라면_레포지토리_풀_리퀘스트_목록에_접근할_수_없다() {
             LocalDateTime now = LocalDateTime.now();
             User user = userGenerator.generate("김건우");
@@ -58,5 +89,7 @@ class RepositoryServiceTest extends BaseServiceTest {
                     .isInstanceOf(GssException.class)
                     .hasMessage(ErrorCode.REPOSITIORY_NOT_FOUND.getMessage());
         }
+
+
     }
 }
