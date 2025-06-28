54a39db12821c69eff05c0a83370f864ed95545a
coli <kkwoo001021@naver.com>
2025-06-28 21:15:35 +0900

    refactor: 문서화 코드 추가

--- gss-api-app/src/main/java/com/devoops/dto/response/UserTokenRefreshResponse.java
+++ gss-api-app/src/main/java/com/devoops/dto/response/UserTokenRefreshResponse.java
@@ -3,8 +3,10 @@ package com.devoops.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserTokenRefreshResponse(
-        @Schema()
+        @Schema(description = "깃허브 회원 아이디", example = "refershAccessToken")
         String accessToken,
+
+        @Schema(description = "재발급한 리프레시 토큰", example = "refreshRefreshToken")
         String refreshToken
 ) {
 
