f9d89b0ba89913c84e0678e6bccc6d327a77b9a6
coli <kkwoo001021@naver.com>
2025-07-10 05:08:30 +0900

    feat: RefreshToken 생성자 추가

--- gss-domain/src/main/java/com/devoops/domain/entity/auth/RefreshToken2.java
+++ gss-domain/src/main/java/com/devoops/domain/entity/auth/RefreshToken2.java
@@ -2,6 +2,7 @@ package com.devoops.domain.entity.auth;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +15,17 @@ public class RefreshToken2 {
     private final long userId;
     private final String value;
     private final LocalDateTime expiredAt;
+
+    public RefreshToken2(long userId) {
+        this.userId = userId;
+        this.value = UUID.randomUUID().toString();
+        this.expiredAt = LocalDateTime.now().plus(REFRESH_TOKEN_TTL);
+    }
+
+    public boolean isExpired() {
+        LocalDateTime now = LocalDateTime.now();
+        return expiredAt.isBefore(now);
+    }
 
     public Duration getTtl() {
         return REFRESH_TOKEN_TTL;
