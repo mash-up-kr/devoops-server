b8d3aa58024d3b2cbc5fb6208dcb6bb2c1845c40
bbeomgeun <miki30808@gmail.com>
2025-07-13 02:02:56 +0900

    refactor: GET -> POST로 웹훅 API 변경

--- gss-mcp-app/src/main/java/com/devoops/controller/webhook/WebhookController.java
+++ gss-mcp-app/src/main/java/com/devoops/controller/webhook/WebhookController.java
@@ -4,7 +4,7 @@ import com.devoops.dto.request.GitHubWebhookEventRequest;
 import com.devoops.service.webhook.WebhookFacadeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ public class WebhookController {
     /**
      * webhook url : https://mcp.dev-oops.kr/api/webhooks/github/pull-requests
      */
-    @GetMapping("/github/pull-requests")
+    @PostMapping("/github/pull-requests")
     public ResponseEntity<Void> getWebhookPullRequest(
             @RequestBody GitHubWebhookEventRequest gitHubWebhookEventRequest
     ) {
