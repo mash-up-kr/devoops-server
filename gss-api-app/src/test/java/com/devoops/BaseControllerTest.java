08d97e701fa0b771cb56e3e82ec3c26f1ca6b18f
coli <kkwoo001021@naver.com>
2025-07-15 03:11:57 +0900

    test: 불필요한 빈 설정 제거

--- gss-api-app/src/test/java/com/devoops/BaseControllerTest.java
+++ gss-api-app/src/test/java/com/devoops/BaseControllerTest.java
@@ -1,9 +1,5 @@
 package com.devoops;
 
-import com.devoops.config.TestConfig;
-import com.devoops.domain.repository.github.GithubRepoDomainRepository;
-import com.devoops.domain.repository.github.PullRequestDomainRepository;
-import com.devoops.domain.repository.user.UserDomainRepository;
 import com.devoops.generator.AnswerGenerator;
 import com.devoops.generator.GithubRepoGenerator;
 import com.devoops.generator.PullRequestGenerator;
@@ -16,11 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@Import(TestConfig.class)
 @ExtendWith(DataBaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseControllerTest {
--- gss-api-app/src/test/java/com/devoops/BaseServiceTest.java
+++ gss-api-app/src/test/java/com/devoops/BaseServiceTest.java
@@ -1,6 +1,5 @@
 package com.devoops;
 
-import com.devoops.config.TestConfig;
 import com.devoops.generator.AnswerGenerator;
 import com.devoops.generator.AnswerRankingGenerator;
 import com.devoops.generator.GithubRepoGenerator;
@@ -10,11 +9,9 @@ import com.devoops.generator.UserGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({"test", "ci"})
-@Import(TestConfig.class)
 @ExtendWith(DataBaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public abstract class BaseServiceTest {
