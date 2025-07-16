dd04e214a6399e4a0c9c0f613fa8934d79500c0c
coli <kkwoo001021@naver.com>
2025-07-17 07:38:40 +0900

    feat: deleteMapping 추가

--- gss-api-app/src/main/java/com/devoops/controller/repository/RepositoryController.java
+++ gss-api-app/src/main/java/com/devoops/controller/repository/RepositoryController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -76,7 +77,11 @@ public class RepositoryController implements RepositoryControllerSwagger {
     }
 
     @Override
-    public ResponseEntity<Void> deleteRepositories(User user, long repositoryId) {
+    @DeleteMapping("/{repositoryId}")
+    public ResponseEntity<Void> deleteRepositories(
+            User user,
+            @PathVariable(name = "repositoryId") long repositoryId
+    ) {
         repositoryFacadeService.deleteRepository(user, repositoryId);
         return ResponseEntity.noContent().build();
     }
