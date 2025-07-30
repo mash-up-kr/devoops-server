b4f657405db3ffca89c144e07b29b965427a7e6a
coli <kkwoo001021@naver.com>
2025-07-30 15:22:44 +0900

    docs: 문서화 코드 작성

--- gss-api-app/src/main/java/com/devoops/dto/response/MyRepositoriesResponse.java
+++ gss-api-app/src/main/java/com/devoops/dto/response/MyRepositoriesResponse.java
@@ -2,10 +2,13 @@ package com.devoops.dto.response;
 
 import com.devoops.domain.entity.github.GithubRepository;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record MyRepositoriesResponse(
-    List<RepositorySummary> repositories
+        @ArraySchema(schema = @Schema(description = "레포지토리 내역", implementation = RepositorySummary.class))
+        List<RepositorySummary> repositories
 ) {
     public static MyRepositoriesResponse from(List<GithubRepository> repositories) {
         return new MyRepositoriesResponse(
@@ -16,10 +19,17 @@ public record MyRepositoriesResponse(
     }
 
     public record RepositorySummary(
-        long id,
-        String name,
-        boolean isTracking,
-        int pullRequestCount
+            @Schema(description = "레포지토리 아이디", example = "1")
+            long id,
+
+            @Schema(description = "레포지토리 이름", example = "my-repo")
+            String name,
+
+            @Schema(description = "레포지토리 추적 여부", example = "true")
+            boolean isTracking,
+
+            @Schema(description = "레포지토리 풀 리퀘스트 개수", example = "10")
+            int pullRequestCount
     ) {
         public static RepositorySummary from(GithubRepository repository) {
             return new RepositorySummary(
