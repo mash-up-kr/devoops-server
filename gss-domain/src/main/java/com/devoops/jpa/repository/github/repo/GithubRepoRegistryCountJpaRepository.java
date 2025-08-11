9d6bbae45333e219cae52b684d59828a4b6433d4
coli <kkwoo001021@naver.com>
2025-08-12 00:55:45 +0900

    chore: registrycount 정보 관리

--- gss-domain/src/main/java/com/devoops/jpa/repository/github/repo/GithubRepoRegistryCountJpaRepository.java
+++ gss-domain/src/main/java/com/devoops/jpa/repository/github/repo/GithubRepoRegistryCountJpaRepository.java
@@ -1,9 +1,8 @@
 package com.devoops.jpa.repository.github.repo;
 
-import com.devoops.domain.entity.github.repo.GithubRepoRegistryCount;
+import com.devoops.jpa.entity.github.repo.GithubRepoRegistryCountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
-public interface GithubRepoRegistryCountJpaRepository extends JpaRepository<GithubRepoRegistryCount, Long> {
+public interface GithubRepoRegistryCountJpaRepository extends JpaRepository<GithubRepoRegistryCountEntity, Long> {
 
 }
--- gss-domain/src/main/java/com/devoops/jpa/repository/github/repo/GithubRepoRegistryCountRepositoryImpl.java
+++ gss-domain/src/main/java/com/devoops/jpa/repository/github/repo/GithubRepoRegistryCountRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.devoops.jpa.repository.github.repo;
 
 import com.devoops.domain.repository.github.repo.GithubRepoRegistryCountRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class GithubRepoRegistryCountRepositoryImpl implements GithubRepoRegistryCountRepository {
 
 }
