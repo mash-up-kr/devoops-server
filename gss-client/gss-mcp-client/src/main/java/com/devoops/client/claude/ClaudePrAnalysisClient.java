a2c5f1d6be8756837a87ddd7a19c506a659714a9
coli <kkwoo001021@naver.com>
2025-08-27 23:30:18 +0900

    feat: client 빈 등록 순서 결정

--- gss-client/gss-mcp-client/src/main/java/com/devoops/client/claude/ClaudePrAnalysisClient.java
+++ gss-client/gss-mcp-client/src/main/java/com/devoops/client/claude/ClaudePrAnalysisClient.java
@@ -13,10 +13,8 @@ import org.springframework.ai.chat.metadata.Usage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.converter.BeanOutputConverter;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 public class ClaudePrAnalysisClient implements PrAnalysisClient {
 
     private final ChatClient chatClient;
@@ -44,7 +42,6 @@ public class ClaudePrAnalysisClient implements PrAnalysisClient {
                 request.codeDifference(),
                 anthropicChatOptions
         );
-        //claude-sonnet-4-20250514
         Usage usage = chatresponse.getMetadata().getUsage();
         String analysisResult = chatresponse.getResult().getOutput().getText();
         PrAnalysis prAnalysis = prAnalysisMapper.mapToPrAnalysis(analysisResult);
@@ -68,7 +65,6 @@ public class ClaudePrAnalysisClient implements PrAnalysisClient {
     ) {
         String userPrompt = promptBuilder.buildUserPrompt(title, description, codeDifference);
         String systemPrompt = promptBuilder.buildSystemPromptWithResponseFormat(outputJsonSchema());
-        log.info("systemPrompt: {}", systemPrompt);
         return chatClient.prompt()
                 .options(options)
                 .system(systemPrompt)
